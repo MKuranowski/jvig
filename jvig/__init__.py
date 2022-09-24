f51cd7f64ec219a49034e04263ef58b39da90334
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-09-24 20:13:32 +0900

    1.0.0 release

---
+++ jvig/__init__.py
@@ -0,0 +1,9 @@
+__title__ = "jvig"
+__description__ = "GTFS Viewer, created using Flask."
+__version__ = "1.0.0"
+__url__ = "https://github.com/MKuranowski/jvig"
+__author__ = "Mikołaj Kuranowski"
+__email__ = "mkuranowski+pypackages@gmail.com"
+
+__copyright__ = "© Copyright 2020-2022 Mikołaj Kuranowski"
+__license__ = "GNU General Public License v3.0 or later"
