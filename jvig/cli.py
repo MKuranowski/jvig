016a74204aad2a68e95b63cf57ba481ca16a9036
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-01-03 13:16:23 +0100

    Update copyright year

--- jvig/cli.py
+++ jvig/cli.py
@@ -1,5 +1,5 @@
 # jvig - GTFS Viewer, created using Flask.
-# Copyright © 2022-2023 Mikołaj Kuranowski
+# Copyright © 2022-2024 Mikołaj Kuranowski
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
